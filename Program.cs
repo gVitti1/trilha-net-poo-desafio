e828a6ad60c670c609c16a88d4338707c13a73eb
gVitti1 <gabrielvitti931@gmail.com>
2023-11-28 21:34:02 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia1 = new Nokia("1234", "M1", "1111111", 64);
+Iphone iphone1 = new Iphone("5678", "M2", "2222222", 128);
+
+Console.WriteLine("Smartphone Nokia: ");
+
+nokia1.Numero = "4321";
+nokia1.Ligar();
+nokia1.InstalarAplicativo("Banco Digital");
+
+Console.WriteLine("Smartphone Iphone: ");
+
+iphone1.Numero = "8765";
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Xadrez Online");
